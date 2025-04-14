9bec84735280e0654c805ff1fac63c3de2c820d5
Moksleivis <Moksleivis@kitm.local>
2025-04-14 14:21:08 +0300

    blank

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
 import{useEffect, useState} from'react'
 import './App.css'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Task from './components/Task'
-import New_task from './components/New_task'
+
 
 function App() {
   
-  const[tasks, setTask] = useState(
-    JSON.parse(localStorage.getItem('tasks')) ||[]
-  );
-
-  useEffect(() => {
-
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-
-  }, [tasks]);
-
   return (
     <>
-    <Navbar/>
-    {/* <Hero/> */}
-
-    <New_task setTasks={setTask} />
-    <Task tasks={tasks} setTasks={setTask} />
     
 
     </>
